733f3ad5dc1a745a612f8a0e44e8640fe97708e2
Gregor Martynus <39992+gr2m@users.noreply.github.com>
2021-11-19 15:57:43 -0800

    useContext: simple Counter (#7)
    * test
    
    * exercise
    
    * extra 3
    
    * learnings
    
    * extra 2

--- src/__tests__/03.extra-1.js
+++ src/__tests__/03.extra-1.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import App from '../exercise/03'
+import App from '../exercise/03.extra-1'
 
 test('clicking the button increments the count', () => {
   render(<App />)
